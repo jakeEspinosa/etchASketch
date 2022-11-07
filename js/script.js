e536fccb1cb70ce7b4e8e3f401ba974f2d4e91bb
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2022-11-06 19:01:51 -0700

    Remove test code and create 16 rows.
    Removed the code applied to each file to test functionality. Added code
    to create 16 divs to lay the groundwork for the 16x16 grid.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('Hello, world!');
+const gridDiv = document.getElementById('grid-div');
+
+for (let i = 1; i < 17; i++) {
+  const newRow = document.createElement('div');
+  newRow.className = 'row';
+  gridDiv.appendChild(newRow);
+}
