0671d8b8961c3d1b93356dc622eae3a0693effa2
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2022-11-07 21:29:40 -0700

    Modularize code.
    Placed code chunks in functions to facilitate readability and for later
    use.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,45 @@
+
+
 const gridDiv = document.getElementById('grid-div');
 
-for (let i = 1; i < 17; i++) {
-  const newRow = document.createElement('div');
-  newRow.className = 'row';
-  gridDiv.appendChild(newRow);
-}
-
-const nodeList = gridDiv.querySelectorAll('.row');
-
-for (const child of nodeList) {
-  for (i = 1; i < 17; i++) {
-    const newBox = document.createElement('div');
-    newBox.className = 'square';
-    child.appendChild(newBox);
+function createRows(rows){
+  for (let i = 1; i < rows + 1; i++) {
+    const newRow = document.createElement('div');
+    newRow.className = 'row';
+    gridDiv.appendChild(newRow);
   }
 }
 
-const boxes = document.querySelectorAll('.square');
+function populateRows(squares) {
+  const rowList = gridDiv.querySelectorAll('.row');
 
-boxes.forEach(box => {
-  box.addEventListener('mouseover', (hoveredBox) => {
-    hoveredBox.currentTarget.classList.add('hoveredOver');
+  for (const child of rowList) {
+    for (i = 1; i < squares + 1; i++) {
+      const newBox = document.createElement('div');
+      newBox.className = 'square';
+      child.appendChild(newBox);
+    }
+  }
+}
+
+function etchASketch() {
+  const boxes = document.querySelectorAll('.square');
+
+  boxes.forEach(box => {
+    box.addEventListener('mouseover', (hoveredBox) => {
+      hoveredBox.currentTarget.classList.add('hoveredOver');
+    });
   });
-});
+}
+
+function initializeGrid(size) {
+  createRows(size);
+  populateRows(size);
+  etchASketch();
+}
+
+initializeGrid(16);
+
+const resizeButton = document.querySelector('#resize');
+
+resizeButton.addEventListener('click', resizeGrid);
